4cf462d6b5d435d4319a896772ec1655cc339d27
payhelpa <dev@payhelpa.com>
2023-09-17 01:07:29 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -137,9 +137,9 @@ const vectorDimension = 1536;
   });
   
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
